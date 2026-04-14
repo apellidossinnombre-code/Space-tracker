c0b9084501c5ed32d37366139dac0530cb05a490
apellidossinnombre-code <apellidossinnombre@gmail.com>
2026-04-14 14:12:46 +0200

    Replace loadList with initList in router

--- js/router.js
+++ js/router.js
@@ -1,12 +1,14 @@
-import { loadList } from "./list.js";
+import { initList } from "./list.js";
 import { renderDetail } from "./detail.js";
 
 export function router(app) {
   const hash = location.hash;
 
   if (!hash || hash === "#/") {
-    loadList(app);
-  } else if (hash.startsWith("#/launch/")) {
+    initList(app);
+  }
+
+  if (hash.startsWith("#/launch/")) {
     const id = hash.split("/")[2];
     renderDetail(id, app);
   }
