e0e6f3fa4d3790cfe055603a9f51ef32736e0a8d
apellidossinnombre-code <apellidossinnombre@gmail.com>
2026-04-14 14:14:19 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-import { router } from "./router.js";
-
-const app = document.getElementById("app");
-
-window.addEventListener("hashchange", () => router(app));
-window.addEventListener("load", () => router(app));
--- js/storage.js
+++
@@ -1,8 +0,0 @@
-export function setCache(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
-}
-
-export function getCache(key) {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : null;
-}
